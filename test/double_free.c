43bebe81104e7f6771f442dfacab52ff0b74b611
lyz_cs <lyz_cs@pku.edu.cn>
2019-11-18 22:41:00 +0800

    fix git format

--- test/double_free.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    int* p = (int *)malloc(sizeof(int));
-    *p = 1;
-    printf("%d", *p);
-    free(p);
-    free(p);
-}
--- tools/flair70/startup/arm/startup.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-sigmake -s -f0 -a0 -o0 "-nStartups of PE files" pe* pe
-
-zipsig	pe
